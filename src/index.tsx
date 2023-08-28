050cadf1ce86ac72ca77346dc486d309b492a843
losidk <losidkl@gmail.com>
2023-08-28 08:44:59 +0800

    feat: title 适配小屏幕

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React, { Suspense } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 
-import reportWebVitals from "./reportWebVitals"
 import { router } from "./router"
 import "./reset.less"
 // import "./mock"
@@ -26,4 +25,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
